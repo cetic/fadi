2127b3179c82e3de031eaa370dd1dbc42bca90eb
Alexandre Nuttinck <alexandre.nuttinck@cetic.be>
2019-07-08 15:30:25 +0200

    Feature/fadi helm chart (#9)
    * Helm Chart for fadi, first commit #7
    * update default values.json, deployment scripts
    * test circle ci script
    * update publish.sh
    * use cetic/fadi helm chart

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -26,27 +26,11 @@ kubectl rollout status deployment tiller-deploy --namespace tiller
 
 printf "\n\nHelm all the things!...\n"
 
-# add helm repos
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+# add cetic helm repo
 helm repo add cetic https://cetic.github.io/helm-charts/
 helm repo update
 
-# spark (+ zeppelin, set to 0 for the moment)
-helm upgrade --install ${NAMESPACE}-spark stable/spark -f ./spark/config.yml --namespace ${NAMESPACE} --tiller-namespace tiller
-# superset
-helm upgrade --install ${NAMESPACE}-superset stable/superset -f ./superset/config.yml --namespace ${NAMESPACE} --tiller-namespace tiller
-# postgres
-helm upgrade --install ${NAMESPACE}-postgres stable/postgresql -f ./postgresql/config.yml --namespace ${NAMESPACE} --tiller-namespace tiller
-# minio
-helm upgrade --install ${NAMESPACE}-minio stable/minio -f ./minio/config.yml --namespace ${NAMESPACE} --tiller-namespace tiller
-# jupyter-hub:
-helm upgrade --install ${NAMESPACE}-jhub jupyterhub/jupyterhub --version=0.8.2 -f ./jupyterhub/config.yml --namespace ${NAMESPACE} --tiller-namespace tiller
-# nifi
-helm upgrade --install ${NAMESPACE}-nifi cetic/nifi -f ./nifi/config.yml --namespace ${NAMESPACE} --tiller-namespace tiller
-# grafana
-helm upgrade --install ${NAMESPACE}-grafana stable/grafana -f ./grafana/config.yml --namespace ${NAMESPACE} --tiller-namespace tiller
-# pg4admin: 
-helm upgrade --install ${NAMESPACE}-pgadmin cetic/pgadmin -f ./pgadmin/config.yml --namespace ${NAMESPACE} --tiller-namespace tiller
+# install/upgrade FADI
+helm upgrade --install ${NAMESPACE} cetic/fadi -f ./values.yaml --namespace ${NAMESPACE} --tiller-namespace tiller
 
 printf "\n\nInstallation successful!\n"
--- helm/pgadmin/README.md
+++
@@ -1,7 +0,0 @@
-# PgAdmin
-
-To install or upgrade pgAdmin:
-
-```
-helm upgrade --install fadi-pgadmin cetic/pgadmin -f helm/pgadmin/config.yml --namespace fadi --tiller-namespace tiller
-```
