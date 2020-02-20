d2b9c04a8c47ade15f5b9cb49434a8b645b6cbec
Faiez Zalila <faiez.zalila@cetic.be>
2020-02-20 14:06:47 +0000

    Update configuration.

--- tests/lib/config.js
+++ tests/lib/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     //baseUrl: 'http://zero.webappsecurity.com/index.html',
-    isHeadless: true,
-    slowMo: 200,
+    isHeadless: false,
+    slowMo: 100,
     isDevtools: false,
     launchTimeout: 10000,
-    waitingTimeout: 100000,
+    waitingTimeout: 10000,
     viewportWidth: 1000,
     viewportHeight: 1000,
 }
