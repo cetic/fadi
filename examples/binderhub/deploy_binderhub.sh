9c6a69e6ea20f39d1b5f26e8448c66fc0a57b8bc
Sebastien Dupont <sebastien.dupont@cetic.be>
2020-05-17 16:07:47 +0200

    Review BinderHub documentation - leftovers

--- examples/binderhub/deploy_binderhub.sh
+++ examples/binderhub/deploy_binderhub.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# this script will deploy the various FADI services on a Kubernetes cluster using Helm and kubectl
-# usage: ./deploy.sh [namespace]
+# This script will deploy the various FADI services on a Kubernetes cluster using Helm and kubectl
+# See https://github.com/cetic/fadi/examples/binderhub/ for usage documentation
+# Usage: ./deploy_binderhub.sh [namespace]
 set -o errexit
 
 LOG_FILE="deploy.log"
@@ -8,21 +9,21 @@ LOG_FILE="deploy.log"
 exec > >(tee -a ${LOG_FILE} )
 exec 2> >(tee -a ${LOG_FILE} >&2)
 
-printf "\n\nFadi is deployed...Now helm will Install Binderhub Around FADI...\n"
-# install/binderhub around FADI
+printf "\n\nFadi is deployed... Now helm will Install BinderHub in FADI...\n"
+# Install BinderHub in FADI
 kubectl get namespace binderhub 2> /dev/null || kubectl create namespace binderhub
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 helm upgrade --install binderhub jupyterhub/binderhub --version=0.2.0-n132.h1a8ce62  -f ./config.yaml --namespace binderhub
 
 sleep 5s
-# Get the node IP where jupyterhub is deployed
+# Get the node IP where JupyterHub is deployed
 nodeIP=$(kubectl get po -n binderhub -o wide |sed -n '/proxy/p' |awk '{ print $7 }')
 if [ $nodeIP = "minikube" ];then
   nodeIP=$(minikube ip)
 fi
 
-#Upgrade the binderhub release with hub_url
-printf "\n\n Find jupyterhub deployed at $nodeIP\n"
+# Upgrade the BinderHub release with hub_url
+printf "\n\n Found JupyterHub deployed at $nodeIP\n"
 helm upgrade binderhub jupyterhub/binderhub --version=0.2.0-n132.h1a8ce62 -f ./config.yaml --set config.BinderHub.hub_url=http://$nodeIP:30902 --namespace binderhub
-printf "\n\nInstallation successful!\n"
+printf "\n\nInstallation successful, !\n"
