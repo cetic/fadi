2ba6d02262473121e35c70ed9aa25b0f92d4ad6f
Faiez Zalila <faiez.zalila@cetic.be>
2020-10-25 14:14:48 +0000

    fix timeout and nifi template path

--- tests/lib/config.js
+++ tests/lib/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    NifiTemplatePath: '../tests/files/basic_example_final_template.xml',
+    NifiTemplatePath: '../examples/basic/basic_example_final_template.xml',
     GrafanaDashboardPath: '../tests/files/USERGUIDE_Dashboard-1581604268718.json',
     isHeadless: true,
     slowMo: 100,
     isDevtools: false,
-    launchTimeout: 10000,
-    waitingTimeout: 10000,
+    launchTimeout: 20000,
+    waitingTimeout: 20000,
     viewportWidth: 1000,
     viewportHeight: 1000,
 }
